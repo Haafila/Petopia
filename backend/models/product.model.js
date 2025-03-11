7ea12aec2c95367605e5493bd13bdbb0e6a5e0d9
Ummu Salma M. H. <ummusalmahasmy@gmail.com>
2025-03-11 17:09:42 +0530

    Product entity added

---
+++ backend/models/product.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+    {
+        name: { 
+            type: String, 
+            required: true 
+        },
+        description: { 
+            type: String 
+        },
+        category: { 
+            type: String, 
+            required: true 
+        },
+        price: { 
+            type: Number, 
+            required: true 
+        },
+        quantity: { 
+            type: Number, 
+            required: true 
+        },
+        imageUrl: { 
+            type: String, 
+            required: true 
+        }, 
+  
+    },
+    {
+        timestamps: true, // createdAt, updatedAt
+    }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
